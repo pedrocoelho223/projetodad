2bd54fccd1b6af9d95c9c49b18739e0670670227
FilipaCapucho8 <2230971@my.ipleiria.pt>
2025-11-27 01:42:28 +0000

    commit anterior v2
    Falta por a funcionar:
    Login e ver lista de jogos

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,21 +1,25 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { io } from 'socket.io-client'
-
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 
+// Ler as variáveis de ambiente
 const apiDomain = import.meta.env.VITE_API_DOMAIN
 const wsConnection = import.meta.env.VITE_WS_CONNECTION
 
 console.log('[main.js] api domain', apiDomain)
 console.log('[main.js] ws connection', wsConnection)
 
+axios.defaults.withCredentials = true;
+
 const app = createApp(App)
 
 app.provide('socket', io(wsConnection))
-app.provide('serverBaseURL', `http://${apiDomain}`)
-app.provide('apiBaseURL', `http://${apiDomain}/api`)
+
+app.provide('serverBaseURL', apiDomain)
+app.provide('apiBaseURL', `${apiDomain}/api`)
 
 app.use(createPinia())
 app.use(router)
